d90a6ca9c64d775028a94d9c6ef5cbec777ab69b
hbadams <channell350@gmail.com>
2022-05-19 14:43:13 -0400

    Update appscript.js

--- src/appscript.js
+++ src/appscript.js
@@ -29,6 +29,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -38,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "efbc00ee87d7e03cc94c3e2bacb189e4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${apiKey}&units=metric`;
+let city = "Boston";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
