193f71ce79ebbc6499a87f0597ae41928a806067
hbadams <channell350@gmail.com>
2022-05-19 12:12:57 -0400

    Adding weather API
    Added live API for city, weather temperature, type, humidity,  and wind speed

--- src/appscript.js
+++ src/appscript.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "efbc00ee87d7e03cc94c3e2bacb189e4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
